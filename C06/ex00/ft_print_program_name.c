357feed2398dda1c32dfb617d881a1582ad37f94
rwallier <rwallier.contato@gmail.com>
2022-02-17 18:35:15 +0100

    C06 success with 70%

---
+++ C06/ex00/ft_print_program_name.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rwallier <rwallier@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 03:45:28 by rwallier          #+#    #+#             */
+/*   Updated: 2022/02/16 03:45:28 by rwallier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str);
+
+int	main(int argc, char *argv[])
+{	
+	if (argc <= 0)
+		return (1);
+	ft_putstr(argv[0]);
+	return (0);
+}
+
+void	ft_putstr(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		write(1, &str[index], 1);
+		index++;
+	}
+	write(1, "\n", 1);
+}
---
+++ C06/ex02/ft_rev_params.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rwallier <rwallier@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 03:45:28 by rwallier          #+#    #+#             */
+/*   Updated: 2022/02/16 03:45:28 by rwallier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putrevstr(char *str);
+void	ft_putargv(char **str, int argc);
+int		ft_strlen(char *str);
+
+int	main(int argc, char *argv[])
+{	
+	if (argc <= 1)
+		return (1);
+	ft_putargv(argv, argc);
+	return (0);
+}
+
+void	ft_putstr(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		write(1, &str[index], 1);
+		index++;
+	}
+	write(1, "\n", 1);
+}
+
+void	ft_putargv(char **str, int argc)
+{
+	int	index;
+
+	index = argc - 1;
+	while (index > 0)
+	{
+		ft_putstr(str[index]);
+		index--;
+	}
+}
+
+int	ft_strlen(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != '\0')
+		index++;
+	return (index);
+}
