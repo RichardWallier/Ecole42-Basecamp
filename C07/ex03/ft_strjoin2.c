735fd642e448311bc024d316185bf5448382caaa
Richard Wallier Batista <rwallier@pop-os.localdomain>
2022-04-14 18:26:44 -0300

    adding files

--- C07/ex03/ft_strjoin2.c
+++ C07/ex03/ft_strjoin2.c
@@ -1,4 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index])
+		index++;
+	return (index);
+}
 
 char	*ft_strcat(char *dest, char *src)
 {
@@ -8,15 +19,9 @@ char	*ft_strcat(char *dest, char *src)
 	counter1 = 0;
 	counter2 = 0;
 	while (dest[counter1] != '\0')
-	{
 		counter1++;
-	}
 	while (src[counter2] != '\0')
-	{
-		dest[counter1] = src[counter2];
-		counter1++;
-		counter2++;
-	}
+		dest[counter1++] = src[counter2++];
 	dest[counter1] = '\0';
 	return (dest);
 }
@@ -25,27 +30,40 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 {
 	char	*finalstr;
 	int		index;
+	int	total_len;
 
-	index = 0;
-	finalstr = malloc (sizeof(strs));
-	while(index < size)
+	if (size == 0)
 	{
-		if(*strs[index] && index > 0)
-				ft_strcat(finalstr, sep);
+		finalstr = malloc(1);
+		finalstr[0] = '\0';
+		return (finalstr);
+	}
+	total_len = 0;
+	index = 0;
+	while (index < size)
+		total_len += ft_strlen(strs[index++]);
+	finalstr = malloc (total_len + ft_strlen(sep) * size - 1);
+	if (finalstr == NULL)
+		return (NULL);
+	index = 0;
+	while (index < size)
+	{
+		if (*strs[index] && index > 0)
+			ft_strcat(finalstr, sep);
 		ft_strcat(finalstr, strs[index++]);
 	}
 	return (finalstr);
 }
 
-// #include <stdio.h>
-// #include <string.h>
+#include <string.h>
 
-// int	main()
-// {
-// 	char	*strs[] = {"um", "dois", "tres", "quatro"};
-// 	char	*sep = ", ";
-// 	int		size = 4;
-// 	char *finalstr = ft_strjoin(size, strs, sep);
-// 	puts(finalstr);
-// 	return (0);
-// }
+int	main()
+{
+	char	*strs[] = {"um", "dois", "tres", "4", "2"};
+	char	*sep = ", ";
+	int		size = 5;
+	char *finalstr = ft_strjoin(size, strs, sep);
+	puts(finalstr);
+	free(finalstr);
+	return (0);
+}
